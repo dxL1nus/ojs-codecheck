366c369aa258331f5f8bf0dfc9a44d5ace37e1e3
dxL1nus <linusldx@gmail.com>
2026-02-08 19:50:53 +0100

    Better styling for settings form, github personal access token option, options are now saved #89

--- classes/Constants.php
+++ classes/Constants.php
@@ -26,7 +26,10 @@ class Constants
     /**
      * Plugin settings keys - NEW ADDITIONS
      */
-    public const CODECHECK_ENABLED = 'enabled';
+    public const CODECHECK_ENABLED = 'codecheckEnabled';
+    public const CODECHECK_AUTHOR_ANONYMITY = 'authorAnonymity';
     public const CODECHECK_API_ENDPOINT = 'codecheckApiEndpoint';
     public const CODECHECK_API_KEY = 'codecheckApiKey';
+    public const CODECHECK_GITHUB_PERSONAL_ACCESS_TOKEN = 'githubPersonalAccessToken';
+    public const GITHUB_REGISTER_REPOSITORY = 'githubRegisterRepository';
 }
