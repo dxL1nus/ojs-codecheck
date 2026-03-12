fe485454f0f58a8d4064b192d5256295848f1f13
dxL1nus <linusldx@gmail.com>
2026-03-12 13:48:59 +0100

    Now repositories and codecheckers are in the issue #53

--- classes/CodecheckRegister/CodecheckGithubRegisterIssue.php
+++ classes/CodecheckRegister/CodecheckGithubRegisterIssue.php
@@ -22,15 +22,17 @@ class CodecheckGithubRegisterIssue {
         string $paperTitle,
         string $journalName,
         string $authorString,
-        string $submissionID
+        string $submissionID,
+        array $codecheckers,
+        array $repositories
     ){
         $this->repositoryOwner = $repositoryOwner;
         $this->repository = $repository;
         $this->submissionID = $submissionID;
         $authorString = empty($authorString) ? 'New CODECHECK' : $authorString;
         $this->title = $this->createTitleMarkdown($authorString, $certificateIdentifier);
-        $this->jsonEncodedCodecheckMetadata = $this->createJsonEncodedCodecheckMetadataMarkdown($authorString, $certificateIdentifier, $journalName, $submissionID);
-        $this->body = $this->createBodyMarkdown($paperTitle, $journalName) . "\n" . $this->jsonEncodedCodecheckMetadata;
+        $this->jsonEncodedCodecheckMetadata = $this->createJsonEncodedCodecheckMetadataMarkdown($authorString, $certificateIdentifier, $journalName, $submissionID, $codecheckers, $repositories);
+        $this->body = $this->createBodyMarkdown($paperTitle, $journalName, $repositories) . "\n" . $this->jsonEncodedCodecheckMetadata;
         $this->labels = $this->createLabelsFromVenue($codecheckVenue);
     }
 
@@ -66,15 +68,17 @@ class CodecheckGithubRegisterIssue {
         string $authorString,
         CertificateIdentifier $certificateIdentifier,
         string $journalName,
-        string $submissionID
+        string $submissionID,
+        array $codecheckers,
+        array $repositories
     ): string
     {
         return "<details>\n<summary><h3>JSON encoded CODECHECK metadata</h3></summary>\n\n"
         . "```json\n"
         . "{"
         . "\n\t\"identifier\": \"" . $certificateIdentifier->toStr() . "\","
-        . "\n\t\"repository\": [],"
-        . "\n\t\"codecheckers\": [],"
+        . "\n\t\"repositories\": " . json_encode($repositories) . ","
+        . "\n\t\"codecheckers\": " . json_encode($codecheckers) . ","
         . "\n\t\"links\": [],"
         . "\n\t\"journal\": {\"name\": \"" . $journalName . "\", \"submissionID\": $submissionID},"
         . "\n}"
@@ -84,13 +88,19 @@ class CodecheckGithubRegisterIssue {
 
     private function createBodyMarkdown(
         string $paperTitle,
-        string $journalName
+        string $journalName,
+        array $repositories
     ): string
     {
+        $repoStr = "";
+        foreach ($repositories as $repo) {
+            $repoStr .= "\t- " . $repo . "\n";
+        }
         return "<!-- Provide the title of your published paper or preprint -->\n## " . $paperTitle . "\n\n"
         . "<!-- Provide a link to your published paper or preprint, ideally with a DOI -->\n**Article:**\n\n"
         . "<!-- Information about the Journal in which the paper/ preprint is published -->\n**Journal:** " . $journalName . " *(Submission ID: " . $this->submissionID . ")*\n\n"
-        . "<!-- Provide a link to your code (and data) repository(s) (GitHub, GitLab, etc.) -->\n**Repository:**";
+        . "<!-- Provide a link to your code (and data) repository(s) (GitHub, GitLab, etc.) -->\n**Repositories:**\n"
+        . $repoStr;
     }
 
     private function createLabelsFromVenue(
